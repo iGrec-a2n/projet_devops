ba36b180f2fe1f005c73faf5931c4fae4bd1047f
Yann N'DA <yannnda97@gmail.com>
2024-12-20 15:40:39 +0100

    Connected Front <-> Back

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 
-const app = express()
+const app = express();
+const PORT = 4000;
 
-app.use(express.json())
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
-app.get("/AddItem", (req, res) => {
-    console.log(req.body)
-    res.send("response received: " + req.body)
-})
+app.get("/api/items", (req, res) => {
+    res.json([
+        { id: 1, name: 'Pineapples', quantity: 1 },
+        { id: 2, name: 'Kiwi', quantity: 7 },    
+    ])
+});
 
+app.post('/api/items', (req, res) => {
+    const newItem = req.body;
+    res.status(201).json({ message: 'Item added successfully', item: newItem });
+});
 
-app.listen(4000, () => console.log("server on localhost:4000"));
+app.listen(PORT, () => console.log(`Le server tourne sur http://localhost:${PORT}`));
